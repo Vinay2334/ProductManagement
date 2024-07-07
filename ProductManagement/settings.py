b4c0aa2af5df77c32fffa0b7f1e3103bb1646ff2
Vinay <vinaybhardwaj2334@gmail.com>
2024-07-08 00:15:07 +0530

    action3

--- ProductManagement/settings.py
+++ ProductManagement/settings.py
@@ -126,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+print('envvar')
+print(os.environ.get('DB_HOST'))
