d90b8db137e351682738fb1263f3ca51cb46067b
Vinay <vinaybhardwaj2334@gmail.com>
2024-07-08 22:07:18 +0530

    configured swagger-ui

--- ProductManagement/urls.py
+++ ProductManagement/urls.py
@@ -14,13 +14,34 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from decouple import config
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
+class CustomSpectacularAPIView(SpectacularAPIView):
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, *args, **kwargs)
+
+        if not config('PROD', default=False, cast=bool):
+            return response
+        
+        schema = response.data
+        
+        # Customize the paths in the schema
+        modified_paths = {}
+        for path, path_item in schema['paths'].items():
+            modified_path = f'/production{path}'
+            modified_paths[modified_path] = path_item
+        
+        schema['paths'] = modified_paths
+        response.data = schema
+        
+        return response
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/product/', include('product.urls')),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/', CustomSpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
--- load_env.py
+++ load_env.py
@@ -7,6 +7,7 @@ def update_zappa_settings():
         "DB_NAME": config('DB_NAME'),
         "DB_USER": config('DB_USER'),
         "DB_PASS": config('DB_PASS'),
+        "PROD": config('PROD'),
     }
 
     with open('zappa_settings.json', 'r') as file:
